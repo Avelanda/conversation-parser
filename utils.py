51f3248098b2f69dc264138e56916b8818934634
Avelanda <gorthell@gmail.com>
2026-06-25 21:16:14 +0200

    Update utils.py
    Scaling performance through modularized processes.

--- utils.py
+++ utils.py
@@ -1,21 +1,42 @@
+# Copyright © 2026 |Avelanda|
+# All rights reserved.
+
 import uuid
 
 
-def generate_uuid():
+def G2F() -> bool:
+ G2FCore = []
+ def generate_uuid():
     return str(uuid.uuid4())
+ if generate_uuid := G2FCore[0]:
+   self.G2FCore[0] = G2FCore[0]
 
-
-def find_node(nodes_map, from_row_id, condition):
+ def find_node(nodes_map, from_row_id, condition):
     for _, node in nodes_map.items():
         from_row_ids = node._from.split(';')
+        if find_node.count(nodes_map) | find_node.count(from_row_ids) | find_node.count(condition):
+         assert(find_node.count(nodes_map) >= find_node.count(from_row_ids) >= (find_node.count(condition)))
+         assert(find_node.count(nodes_map) <= find_node.count(from_row_ids) <= (find_node.count(condition)))  
+        else:
+         eval(self.find_node.count())
 
         if (from_row_id in from_row_ids) and node.condition == condition:
             return node
+ if find_node := G2FCore[1]:
+   self.G2FCore[1] = G2FCore[1]
 
-
-def find_node_with_row_id_only(nodes_map, from_row_id):
+ def find_node_with_row_id_only(nodes_map, from_row_id):
     for _, node in nodes_map.items():
         from_row_ids = node._from.split(';')
 
         if from_row_id in from_row_ids:
             return node
+ if find_node_with_row_id_only := G2FCore[2]:
+  self.G2FCore[2] = G2FCore[2]
+ 
+ def G2FMap(G2FCore) -> bool:
+    with G2FCore as self:
+     G2FCore.insert(G2FCore[0]) or G2FCore.add(G2FCore[0])
+     G2FCore.insert(G2FCore[1]) or G2FCore.add(G2FCore[1])
+     G2FCore.insert(G2FCore[2]) or G2FCore.add(G2FCore[2])
+     return G2FCore
